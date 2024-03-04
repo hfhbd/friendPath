b6aeb5e0005945f373261851288e1a6435b5458c
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-03-04 13:28:02 +0100

    Use new Gradle configuration api (#20)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- gradle/build-logic/src/main/kotlin/friendPath.gradle.kts
+++ gradle/build-logic/src/main/kotlin/friendPath.gradle.kts
@@ -2,14 +2,19 @@ plugins {
     kotlin("jvm")
 }
 
-val friendPath by configurations.registering {
+val friendPath = configurations.dependencyScope("friendPath") {
+    isTransitive = false
+}
+
+val friendPathJars = configurations.resolvable("friendPathJars") {
+    extendsFrom(friendPath.get())
     isTransitive = false
     attributes.attribute(
         LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named<LibraryElements>("jar")
     )
 }
 
-val friendPathArgs: Provider<List<String>> = friendPath.flatMap {
+val friendPathArgs: Provider<List<String>> = friendPathJars.flatMap {
     it.elements.map { elements: Set<FileSystemLocation> ->
         elements.map {
             val path = it.asFile.absolutePath
